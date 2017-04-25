bb00280a380e94eb6dac9661947ff1c0c837809a
Caroline Chen <carolinecaroline@gmail.com>
2017-04-25 17:05:57 -0400

    create new house

--- game_of_thrones/app/controllers/characters_controller.rb
+++ game_of_thrones/app/controllers/characters_controller.rb
@@ -5,9 +5,19 @@ def show
 	@character = @house.characters.find(params[:id])
 end
 
+def new
+	@house = Artist.find(params[:house_id])
+	@character = Character.new
+end
 
+def create
+	@house = House.find(params[:house_id])
+	@character = @house.characters.create!(character_params)
+	redirect_to house_character_path(@house, @character)
+end
 
-
-
-
+private
+def character_params
+	params.require(:character).permit(:name, :title, :alias, :image, :house_id)
+end
 end
--- game_of_thrones/app/controllers/houses_controller.rb
+++ game_of_thrones/app/controllers/houses_controller.rb
@@ -8,11 +8,18 @@ def show
 	@house = House.find(params[:id])
 end
 
+def new
+	@house = House.new
+end
+
+def create
+	@house = House.create!(house_params)
+	redirect_to house_path(@house)
+end
 
 
-
-
-
-
-
+private
+def house_params
+	params.require(:house).permit(:name, :sigil, :words, :image)
+end
 end
